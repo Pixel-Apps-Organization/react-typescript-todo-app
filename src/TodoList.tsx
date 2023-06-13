d25840f57e7560341bb888dbadbf020a080afc58
RepoRobot <RepoRobot@reporobot.ai>
2023-06-13 07:19:28 +0000

    Split todos into active and completed sections

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -11,15 +11,31 @@ export const TodoList: React.FC<TodoListProps> = ({
   todos,
   toggleComplete
 }) => {
+  const activeTodos = todos.filter(todo => !todo.complete);
+  const completedTodos = todos.filter(todo => todo.complete);
+
   return (
-    <ul>
-      {todos.map(todo => (
-        <TodoListItem
-          key={todo.text}
-          todo={todo}
-          toggleComplete={toggleComplete}
-        />
-      ))}
-    </ul>
+    <>
+      <h3>Active Todos</h3>
+      <ul>
+        {activeTodos.map(todo => (
+          <TodoListItem
+            key={todo.text}
+            todo={todo}
+            toggleComplete={toggleComplete}
+          />
+        ))}
+      </ul>
+      <h3>Completed Todos</h3>
+      <ul>
+        {completedTodos.map(todo => (
+          <TodoListItem
+            key={todo.text}
+            todo={todo}
+            toggleComplete={toggleComplete}
+          />
+        ))}
+      </ul>
+    </>
   );
 };
